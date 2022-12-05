cbf77359729e24036318e2f603a3b715bbaaad3e
Christopher Harrison <christopher.harrison@tweag.io>
2022-12-05 13:33:09 +0000

    Day 3, Part 1

--- 03/src/main.rs
+++ 03/src/main.rs
@@ -1,3 +1,65 @@
+mod test;
+
+use std::collections::HashSet;
+use std::io;
+
+fn rucksack_compartments(rucksack: &str) -> (&str, &str) {
+    // Split the rucksack string into its equal compartments
+    let len = rucksack.len();
+    assert!(len % 2 == 0);
+
+    (&rucksack[..len / 2], &rucksack[len / 2..])
+}
+
+fn common_item(compartments: (&str, &str)) -> Option<char> {
+    // Find the single common item (if any) contained in each compartment
+    let (left, right) = compartments;
+    let a: HashSet<char> = HashSet::from_iter(left.chars());
+    let b: HashSet<char> = HashSet::from_iter(right.chars());
+    let common: Vec<&char> = a.intersection(&b).collect();
+
+    // common.first() doesn't work because it returns Option<&&char>
+    match common.len() {
+        1 => Some(common[0].clone()),
+        _ => None,
+    }
+}
+
+fn item_priority(item: char) -> u32 {
+    // Convert item characters into their priority values
+    // NOTE We rely on codepoints and the Roman alphabet being ordered sequentially
+    match item {
+        'a'..='z' => (item as u32) - ('a' as u32) + 1,
+        'A'..='Z' => (item as u32) - ('A' as u32) + 27,
+
+        _ => panic!("Unknown item"),
+    }
+}
+
+fn rucksack_priority(rucksack: &str) -> u32 {
+    if let Some(item) = common_item(rucksack_compartments(rucksack)) {
+        item_priority(item)
+    } else {
+        panic!("No common item")
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let stdin = io::stdin();
+    let mut line = String::new();
+
+    let mut sum = 0;
+
+    loop {
+        sum += match stdin.read_line(&mut line) {
+            Ok(0) => break,
+            Ok(_) => rucksack_priority(&line.trim()),
+
+            Err(e) => panic!("{e}"),
+        };
+
+        line.clear();
+    }
+
+    println!("{sum}");
 }
---
+++ 03/src/test.rs
@@ -0,0 +1,37 @@
+#[cfg(test)]
+use crate::*;
+
+#[test]
+fn test_rucksack_compartments() {
+    assert_eq!(rucksack_compartments("a1"), ("a", "1"));
+    assert_eq!(rucksack_compartments("abc123"), ("abc", "123"));
+    assert_eq!(
+        rucksack_compartments("vJrwpWtwJgWrhcsFMMfFFhFp"),
+        ("vJrwpWtwJgWr", "hcsFMMfFFhFp")
+    );
+}
+
+#[test]
+fn test_common_item() {
+    assert_eq!(common_item(("a", "a")), Some('a'));
+    assert_eq!(common_item(("vJrwpWtwJgWr", "hcsFMMfFFhFp")), Some('p'));
+    assert_eq!(common_item(("abc", "ABC")), None);
+}
+
+#[test]
+fn test_item_priority() {
+    assert_eq!(item_priority('a'), 1);
+    assert_eq!(item_priority('z'), 26);
+    assert_eq!(item_priority('A'), 27);
+    assert_eq!(item_priority('Z'), 52);
+}
+
+#[test]
+fn test_rucksack_priority() {
+    assert_eq!(rucksack_priority("vJrwpWtwJgWrhcsFMMfFFhFp"), 16);
+    assert_eq!(rucksack_priority("jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL"), 38);
+    assert_eq!(rucksack_priority("PmmdzqPrVvPwwTWBwg"), 42);
+    assert_eq!(rucksack_priority("wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn"), 22);
+    assert_eq!(rucksack_priority("ttgJtRGJQctTZtZT"), 20);
+    assert_eq!(rucksack_priority("CrZsJsPPZsGzwwsLwLmpwMDw"), 19);
+}
