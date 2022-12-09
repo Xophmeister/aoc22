623a365c2dfc3c0ba97974c6e05c697882cd30bb
Christopher Harrison <christopher.harrison@tweag.io>
2022-12-09 17:28:41 +0000

    Day 7, Part 2

--- 07/src/main.rs
+++ 07/src/main.rs
@@ -8,6 +8,9 @@ use std::path::PathBuf;
 use crate::error::ParseError;
 use crate::stat::Stat;
 
+const DISK_SPACE: u32 = 70000000;
+const NEEDED_SPACE: u32 = 30000000;
+
 fn main() {
     if let Err(e) = run() {
         eprintln!("{e}");
@@ -21,7 +24,7 @@ fn run() -> Result<(), ParseError> {
     // Unique directories attested in session
     let directories: HashSet<&PathBuf> = HashSet::from_iter(stats.iter().map(|stat| stat.path()));
 
-    let part_a: u32 = directories
+    let dir_sizes: Vec<u32> = directories
         .iter()
         .map(|directory| {
             stats
@@ -30,10 +33,26 @@ fn run() -> Result<(), ParseError> {
                 .map(|stat| stat.get_size())
                 .sum()
         })
-        .filter(|size: &u32| *size <= 100000)
+        .collect();
+
+    let used_space: u32 = stats
+        .iter()
+        .filter(|stat| stat.is_file() && stat.is_under(&PathBuf::new()))
+        .map(|stat| stat.get_size())
         .sum();
 
+    let need_to_delete: u32 = NEEDED_SPACE - (DISK_SPACE - used_space);
+
+    let part_a: u32 = dir_sizes.iter().filter(|size| **size <= 100000).sum();
+
+    let part_b: u32 = *dir_sizes
+        .iter()
+        .filter(|size| **size >= need_to_delete)
+        .min()
+        .unwrap();
+
     println!("Part 1: {part_a}");
+    println!("Part 2: {part_b}");
 
     Ok(())
 }
